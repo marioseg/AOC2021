2fda560225d7c5546a59b4f18b8cb280ae4bf619
Nick  Tom Thomas <nickbetty2000@yahoo.com>
2021-12-02 14:29:49 -0500

    Revert "completed chapter 2"
    This reverts commit 2d270d05bceef97cdfa48dfdfca2a806bd11d570.

--- index.js
+++
@@ -1,3 +0,0 @@
-import { main } from "./src/DayZero/index.js";
-
-main();
--- src/DayOne/index.spec.js
+++
@@ -1,8 +0,0 @@
-import { main } from "../DayZero";
-
-describe("Simple test suite", () => {
-  it("loads file", () => {
-    const load = main();
-    expect(load).toEqual(1);
-  });
-});
